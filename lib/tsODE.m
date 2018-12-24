8d584e97fd432d753ed43170a67fe07808dd9f68
hespanha <hespanha@ece.ucsb.edu>
2018-12-24 13:47:15 +0000

    Fixes related to the ZOH input

--- lib/tsODE.m
+++ lib/tsODE.m
@@ -1,81 +1,78 @@
-function constraint=tsODE(x,u,d,ts,fun,method)
-% constraint=tsODE(x,u,d,ts,fun,method)
+function constraint=tsODE(x,uZOH,uC,ts,fun,method)
+% constraint=tsODE(x,uZOH,uC,ts,fun,method)
 %
 % computes a Tcalculus constraint that encodes the ODE
-%     \dot x = f(x,u,d,t)
-% with the understanding that the input 'd' is assumes continuous
-% and the input 'u' is assumed piecewise constant (zero-oerder
+%     \dot x = f(x,uZOH,uC,t)
+% with the understanding that the input 'uC' is assumes continuous
+% and the input 'uZOH' is assumed piecewise constant (zero-order
 % hold).
 %
 % Inputs:
 %   x [n x N]  - values of the state x at sampling times
 %                (one time per column)
-%   u [n x N]  - values of the input u at sampling times
+%   uZOH [n x N]  - values of the input u at sampling times
 %                (one time per column)
-%                u(t) is assumed constant until the next
+%                uZOH(t) is assumed constant until the next
 %                sampling time
 %                Can be an emptuy vector if no such input is needed.
-%   d [n x N]  - values of the input d at sampling times
+%   uC [n x N]  - values of the input d at sampling times
 %                (one time per column)
-%                d(t) is assumed continuous until the next
+%                uC(t) is assumed continuous until the next
 %                sampling time
 %                Can be an emptuy vector if no such input is needed.
 %   ts [N x 1] - vector of times 
 %    or
 %   ts [1 x 1] - (constant) sample interval
 %   fun        - handle to a function that computes 
-%                    f(x,u,d,t)
+%                    f(x,uZOH,uC,t)
 %   method     - ODE integration method: 
 %                  'forwardEuler'  - explicit forward Euler method
 %                  'backwardEuler' - implicit forward Euler method
 %                  'midPoint'      - implicit mid-point method
 %                The mid-point method typically leads to the best
 %                accuracy (for a similar integration step).
-%
-%                The mid-point methid currently ignores the
-%                difference between the inputs u & d (and treats
-%                them both as continuous) because it appears to
-%                lead to large errors for a zoh control input.
+%                HOWEVER, it often leads to numerical problems 
+%                when optimizing for the control input.
 [n,N]=size(x);
 switch method
   case 'forwardEuler'
     if length(ts)>1 
         constraint=(x(:,2:end)==x(:,1:end-1)...
                     +tprod(ts(2:end)-ts(1:end-1),2,...
-                           fun(x(:,1:end-1),u(:,1:end-1),d(:,1:end-1),ts(1:end-1)),[1,2]));
+                           fun(x(:,1:end-1),uZOH(:,1:end-1),uC(:,1:end-1),ts(1:end-1)),[1,2]));
     else
         constraint=(x(:,2:end)==x(:,1:end-1)...
-                    +ts*fun(x(:,1:end-1),u(:,1:end-1),d(:,1:end-1),ts*(0:N-1)'));
+                    +ts*fun(x(:,1:end-1),uZOH(:,1:end-1),uC(:,1:end-1),ts*(0:N-1)'));
     end
   case 'backwardEuler'
     if length(ts)>1 
         constraint=(x(:,2:end)==x(:,1:end-1)...
                     +tprod(ts(2:end)-ts(1:end-1),2,...
-                           fun(x(:,2:end),u(:,1:end-1),d(:,2:end),ts(2:end)),[1,2]));
+                           fun(x(:,2:end),uZOH(:,1:end-1),uC(:,2:end),ts(2:end)),[1,2]));
     else
         constraint=(x(:,2:end)==x(:,1:end-1)...
-                    +ts*fun(x(:,2:end),u(:,1:end-1),d(:,2:end),ts*(1:N)'));
+                    +ts*fun(x(:,2:end),uZOH(:,1:end-1),uC(:,2:end),ts*(1:N)'));
     end
   case 'midPoint'
     % with N data points, one can only have N-1 independent
     % constraints so one constraint should be removed
     lhs=tsDerivative(x,ts);
     lhs=lhs(:,1:end-1);
-    if ~isempty(u)
-        if 0
-            uu=.5*(u(:,1:end-1)+u(:,2:end));
+    if ~isempty(uZOH)
+        if 1
+            uu=.5*(uZOH(:,1:end-1)+uZOH(:,2:end));
             uu=[uu(:,1),uu];
         else
-            fprintf('ATTENTION: tsODE(...,''midpoint'') is currently not doing zero-order hold for input ''u''\n');
+            fprintf('ATTENTION: tsODE(...,''midpoint'') is currently not doing zero-order hold for input ''uZOH''\n');
             uu=u;
         end
     else
-        uu=u;
+        uu=uZOH;
     end
     if length(ts)>1 
-        rhs=fun(x,uu,d,ts);
+        rhs=fun(x,uZOH,uC,ts);
     else
-        rhs=fun(x,uu,d,ts*(1:N)');
+        rhs=fun(x,uZOH,uC,ts*(1:N)');
     end
     rhs=rhs(:,1:end-1);
     constraint=(lhs==rhs);
