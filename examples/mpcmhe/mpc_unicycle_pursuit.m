477371cde1bb455bd6d607f9bbb8b2ded8e1b39c
hespanha <hespanha@ece.ucsb.edu>
2019-10-13 01:21:01 -0700

    Cleaned up example Fixed compatibility with latets Tmpc

--- examples/mpcmhe/mpc_unicycle_pursuit.m
+++ examples/mpcmhe/mpc_unicycle_pursuit.m
@@ -1,4 +1,4 @@
-% Copyright 2012-2017 Joao Hespanha
+% Copyright 2012-2019 Joao Hespanha
 
 % This file is part of Tencalc.
 %
@@ -17,7 +17,7 @@
 
 clear all
 % remove previous solvers
-delete('toremove.m','tmp*');rc=rmdir('@tmp*','s');
+%delete('toremove.m','tmp*');rc=rmdir('@tmp*','s');
 
 %% Generate solver
 
@@ -28,7 +28,6 @@ nu=1;             % control size
 T=200;            % forward horizon;
 controlDelay=0;   % delay in applying control, after state measurement
 
-% create symbolic optimization
 Tvariable Ts [];
 Tvariable x [nx,T];  % [x(t+Ts), x(t+2*Ts), ..., x(t+T*Ts)]
 Tvariable u [nu,T];  % [u(t), u(t+Ts), ..., u(t+(T-1)*Ts) ]
@@ -78,32 +77,35 @@ mpc=Tmpc('reuseSolver',true,...
 %% Simulate system
 
 % set parameter values
-setParameter(mpc,'d',[1;0]);
-setParameter(mpc,'v',2);
-uMax=2;
-setParameter(mpc,'uMax',uMax);
+d=[1;0];v=2;
+setParameter(mpc,'d',d);
+setParameter(mpc,'v',v);
 Ts=.01;
 setParameter(mpc,'Ts',Ts);
+
+uMax=2;
+setParameter(mpc,'uMax',uMax);
 
 % set process initial condition
 t0=0;
 x0=[-1;-1;0;0;0];
 setInitialState(mpc,t0,x0);
 
-fig=1;
-figure(fig);clf;
-
-mu0=1;
-maxIter=20;
-saveIter=false;
-
-t=t0;
-figure(fig);clf;
-
 % cold start
 u_warm=.1*randn(nu,T-controlDelay);               
 
-for i=1:400
+mu0=1;
+maxIter=50;
+saveIter=false;
+
+nSteps=1000;
+
+fig=1;
+figure(fig);clf;
+set(fig,'Name','MPC solutions (Tmpc)');
+
+t=t0;
+for i=1:nSteps
     % move warm start away from constraints
     u_warm=min(u_warm,.95*uMax);
     u_warm=max(u_warm,-.95*uMax);
@@ -111,11 +113,20 @@ for i=1:400
     
     [solution,J,x,u]=solve(mpc,mu0,maxIter,saveIter);
     
-    fprintf('t=%g, J=%g computed in %g iterations & %g ms\n',t,J,solution.iter,1e3*solution.time);
+    if solution.status==0
+        fprintf('t=%g, J=%g computed in %g iterations & %g ms\n',t,J,solution.iter,1e3*solution.time);
+    else
+        fprintf('t=%g, J=%g computed in %g iterations & %g ms\n',t,J,solution.iter,1e3*solution.time);
+        disp(solution);
+        warning('solver failed')
+    end
     
-    if mod(i,5)==0
+    if mod(i,10)==0
         plot(x(1,:),x(2,:),'k.-',...
-             x(4,:),x(5,:),'b.-');
+             x(4,:),x(5,:),'b.-',...
+             x(1,1),x(2,1),'r*',...
+             x(4,1),x(5,1),'g*'...
+             );
         axis equal;
         grid on;
         legend('pursuer','evader','location','best');
@@ -125,22 +136,26 @@ for i=1:400
         %pause
     end
     
-    % apply 5 controls and get time, warm start for next iteration
     ufinal=zeros(nu,1);
-    [t,u_warm]=applyControls(mpc,solution,5,ufinal); 
+    [t,u_warm]=applyControls(mpc,solution,ufinal); 
 end
 
 history=getHistory(mpc);
 
 fig=fig+1;figure(fig);clf;
+set(fig,'Name','Closed-loop (Tmpc)');
 plot(history.x(1,:),history.x(2,:),'k.-',...
      history.x(4,:),history.x(5,:),'b.-');
 axis equal;
 grid on;
 legend('pursuer','evader','location','best');
-title('full trajectory');
 
 fig=fig+1;figure(fig);clf;
+set(fig,'Name','Solver (Tmpc)');
+subplot(2,1,1);
+plot(history.t,history.objective,'.-');grid on;
+ylabel('MPC cost');
+subplot(2,1,2);
 yyaxis left
 plot(history.t,history.iter,'.-');grid on;
 ylabel('# iterations');
