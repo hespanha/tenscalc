1c81a6a5b0cf10a4aab40a68aaedd9419e3106ba
hespanha <hespanha@ece.ucsb.edu>
2020-04-20 00:09:37 -0700

    Added documentation

---
+++ lib/logdet.m
@@ -0,0 +1,9 @@
+function y=logdet(A)
+% logdet - Natural logarithm of the determinant of a matrix
+%      
+%   logdet(A) returns the natural logarithm of the determinant of the
+%   square matrix A.
+%
+    y=log(det(A));
+end
+    
--- lib/norm2.m
+++ lib/norm2.m
@@ -1,11 +1,13 @@
 function obj=norm2(obj1,S)
-% y=norm2(x) returns the sum of the square of all entries opf
-% x, which for matrices corresponds to the square of the
-% Frobenius norm of x.
-%
-% y=norm2(x,S) returns the value of the quadratic form
-% <x,Sx>. This form is only applicable when x is a vector
-% (1d-tensor) and S a square matrix (2d-tensor).
+% norm2 - Squared quadratic norm 
+%   
+%   norm2(x) returns the sum of the square of all entries of
+%   the tensor x, which for matrices corresponds to the square
+%   of the Frobenius norm of x.
+%   
+%   norm2(x,S) returns the value of the quadratic form <x,Sx>. This
+%   form is only applicable when x is a vector (tensor with 1
+%   dimension) and S a square matrix (tensor with 2 dimensions).
 
 if nargin<2
     obj=sum(obj1(:).^2);
@@ -18,3 +20,6 @@ else
         error('norm2: norm2(x,S) called with x[%s] not a column vector and/or S[%s] not a square matrix of compatible size\n',index2str(size(obj1)),index2str(size(S)));
     end
 end
+
+end
+
