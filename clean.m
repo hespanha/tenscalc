40237f274b2813f7e51ca24e2168433b53767135
hespanha <hespanha@ece.ucsb.edu>
2020-04-20 00:32:55 -0700

    Added script to remove temp files

---
+++ clean.m
@@ -0,0 +1,6 @@
+clear all;
+
+!rm -r lib/tmp* lib/@tmp* lib/*/tmp* lib/*/@tmp* 
+!rm -r examples/tmp* examples/@tmp* 
+!rm -r examples/*/tmp* examples/*/@tmp* 
+!rm - rexamples/*/*/tmp* examples/*/*/@tmp* 
