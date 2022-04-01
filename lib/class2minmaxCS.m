5b91ca8c18b3e0ae98725be3264a69c271a4e9dc
hespanha <hespanha@ece.ucsb.edu>
2022-04-01 12:44:30 -0700

    Added draft templates for minmax optimizations

---
+++ lib/class2minmaxCS.m
@@ -0,0 +1,376 @@
+function varargout=class2minmaxCS(varargin)
+% To get help, type class2minmaxCS('help')
+%
+% This file is part of Tencalc.
+%
+% Copyright (C) 2012-21 The Regents of the University of California
+% (author: Dr. Joao Hespanha, Raphael Chinchilla).  All rights reserved.
+
+    %% Function global help
+    declareParameter(...
+        'Help', {
+            'Creates a matlab class for solving a min-max optimization'
+            '(Stackelberg equilibrium) of the form'
+            '  objective(minVariables^*,maxVariables^*,parameters) ='
+            '        = minimize                                             maximize    objective(minVariables,maxVariables,parameters)'
+            '          w.r.t.      minVariables                             w.r.t.       maxVariables'
+            '          subject to  minConstraints(minVariables,parameters)  siubject to  maxConstraints(minVariables,maxVariables,parameters)'
+            'and returns'
+            '  outputExpressions(minVariables^*,maxVariables^*,parameters)'
+            ' '
+            'See TO_BE_FIXED for details of the optimization engine.'
+            ' '
+            'The solver is accessed through a matlab class.'
+                });
+
+    localVariables_=parameters4minmax(localVariables_);
+
+    localVariables_=parameters4all(localVariables_);
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Retrieve parameters and inputs
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    [stopNow,params]=setParameters(nargout,varargin);
+    if stopNow
+        return
+    end
+
+    %% transfer any folder in classname into folder
+    [folder,classname]=fileparts(fsfullfile(folder,classname));
+
+    %% create folder if it does not exist
+    if ~strcmp(folder,'.') && ~exist(folder,'dir')
+        fprintf('class2minmaxCS: outputs folder ''%s'' does not exist, creating it\n',folder);
+        if mkdir(folder)==0
+            error('Unable to create folder ''%s''\n',folder)
+        end
+    end
+
+    rmpath(folder);
+    addpath(folder);
+
+    %% Fix class when gotten from pedigree
+    classname=regexprep(classname,'+TS=','_TS_');
+    classname=regexprep(classname,'-','_');
+    classname=regexprep(classname,'+','_');
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Check input parameters
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    parameters=checkParameters(parameters);
+
+    if isstruct(minOptimizationVariables)
+        minOptimizationVariables=struct2cell(minOptimizationVariables);
+    end
+
+    if isstruct(maxOptimizationVariables)
+        maxOptimizationVariables=struct2cell(maxOptimizationVariables);
+    end
+
+    if ~iscell(minOptimizationVariables)
+        minOptimizationVariables
+        error('minOptimizationVariables must be a cell array of Tcalculus variables');
+    end
+
+    for i=1:length(minOptimizationVariables)
+        if ~isequal(class(minOptimizationVariables{i}),'Tcalculus')
+            minOptimizationVariables{i}
+            error('all minOptimizationVariables must be of the type ''variable'' (%dth is of type ''%s'')\n',...
+                  i,class(minOptimizationVariables{i}));
+        end
+        if ~isequal(type(minOptimizationVariables{i}),'variable')
+            minOptimizationVariables{i}
+            error('all minOptimizationVariables must be of the type ''variable'' (%dth is of type ''%s'')\n',...
+                  i,type(minOptimizationVariables{i}));
+        end
+        for j=1:length(parameters)
+            if isequal(name(minOptimizationVariables{i}),name(parameters{j}))
+                minOptimizationVariables{i}
+                error('optimization variable ''%s'' cannot also be a parameter\n',name(minOptimizationVariables{i}));
+            end
+        end
+    end
+
+    if ~iscell(maxOptimizationVariables)
+        maxOptimizationVariables
+        error('maxOptimizationVariables must be a cell array of Tcalculus variables');
+    end
+
+    for i=1:length(maxOptimizationVariables)
+        if ~isequal(class(maxOptimizationVariables{i}),'Tcalculus')
+            minOptimizationVariables{i}
+            error('all minOptimizationVariables must be of the type ''variable'' (%dth is of type ''%s'')\n',...
+                  i,class(maxOptimizationVariables{i}));
+        end
+        if ~isequal(type(maxOptimizationVariables{i}),'variable')
+            maxOptimizationVariables{i}
+            error('all maxOptimizationVariables must be of the type ''variable'' (%dth is of type ''%s'')\n',...
+                  i,type(maxOptimizationVariables{i}));
+        end
+        for j=1:length(parameters)
+            if isequal(name(maxOptimizationVariables{i}),name(parameters{j}))
+                maxOptimizationVariables{i}
+                error('optimization variable ''%s'' cannot also be a parameter\n',name(maxOptimizationVariables{i}));
+            end
+        end
+    end
+
+    if ~isempty(size(objective))
+        error('optimization criterion must be scalar (not [%s])',...
+              index2str(size(objective)));
+    end
+
+    if ~isempty(minConstraints) && ~iscell(minConstraints)
+        error('minimizer''s constraints parameter must be a cell array\n');
+    end
+
+    if ~isempty(maxConstraints) && ~iscell(maxConstraints)
+        error('maximizer''s constraints parameter must be a cell array\n');
+    end
+
+    [outputExpressions,outputNames]=checkOutputExpressions(outputExpressions);
+
+    fprintf('class2minmaxCS: ...');
+    t_classCS=clock();
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Declare the problem-specific variables
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    fprintf(' declaring sets for parameters and primal variables... ');
+
+    t_csparse=clock();
+    debug=false;
+    tprod2matlab=true;
+    code=csparse(scratchbookType,debug,tprod2matlab); % using instructionsTable.c
+    code.LDLthreshold=LDLthreshold;
+    classhelp={'Create object';
+               sprintf('obj=%s();',classname)};
+
+    %% Declare 'sets' for initializing parameters
+    if length(parameters)>0
+        classhelp{end+1}='Set parameters';
+    end
+    for i=1:length(parameters)
+        declareSet(code,parameters{i},sprintf('setP_%s',name(parameters{i})));
+        msize=size(parameters{i});while(length(msize)<2),msize(end+1)=1;end
+        classhelp{end+1}=sprintf('setP_%s(obj,{[%s] matrix});',...
+                                name(parameters{i}),index2str(msize));
+    end
+
+    %% Declare 'sets' for initializing primal variables
+    classhelp{end+1}='Initialize primal variables';
+    % Minimizer
+    for i=1:length(minOptimizationVariables)
+        declareSet(code,minOptimizationVariables{i},...
+                   sprintf('setV_%s',name(minOptimizationVariables{i})));
+        msize=size(minOptimizationVariables{i});while(length(msize)<2),msize(end+1)=1;end
+        classhelp{end+1}=sprintf('setV_%s(obj,{[%s] matrix});',...
+                                 name(minOptimizationVariables{i}),index2str(msize));
+    end
+    % Maximizer
+    for i=1:length(maxOptimizationVariables)
+        declareSet(code,maxOptimizationVariables{i},...
+                   sprintf('setV_%s',name(maxOptimizationVariables{i})));
+        msize=size(maxOptimizationVariables{i});while(length(msize)<2),msize(end+1)=1;end
+        classhelp{end+1}=sprintf('setV_%s(obj,{[%s] matrix});',...
+                                 name(maxOptimizationVariables{i}),index2str(msize));
+    end
+
+    %% Define constraints, dual variables, and declare 'sets' for initializing dual variables
+
+    if verboseLevel>0
+        fprintf('  Defining primal variables, constraints, and dual variables... ');
+    end
+
+    % Minimizer
+    [Gu,Fu,minNus,minLambdas,outputExpressions]=...
+        parseConstraints(code,classname,minConstraints,outputExpressions,'minimizer');
+
+    % Maximizer
+    [Gd,Fd,maxNus,maxLambdas,outputExpressions]=...
+        parseConstraints(code,classname,maxConstraints,outputExpressions,'maximizer');
+
+    %% Pack constraints
+
+    if verboseLevel>1
+        fprintf('Packing expressions and variables... ');
+    end
+
+    %% Pack primal variables
+    % Minimizer
+    [u,~,~,~,objective,outputExpressions,Fu,Gu,Fd,Gd,H]...
+        =packVariables(minOptimizationVariables,'u_',...
+                       objective,outputExpressions,Fu,Gu,Fd,Gd,H);
+    u0=packExpressions(minOptimizationVariables);
+    % Maximizer
+    [d,~,~,~,objective,outputExpressions,Fu,Gu,Fd,Gd,H]...
+        =packVariables(maxOptimizationVariables,'d_',...
+                       objective,outputExpressions,Fu,Gu,Fd,Gd,H);
+    d0=packExpressions(maxOptimizationVariables);
+    src={u0,d0};
+    dst={u,d};
+
+    declareCopy(code,dst,src,'initPrimal__');
+
+    %% Pack dual variables
+    % Minimizer
+    if size(Gu,1)>0
+        minNu=packVariables(minNus,'minNu_');
+        minNu0=packExpressions(minNus);
+        src{end+1}=minNu0;
+        dst{end+1}=minNu;
+    else
+        minNu=Tzeros(0);
+    end
+    if size(Fu,1)>0
+        minLambda=packVariables(minLambdas,'minLambda_');
+        minLambda0=packExpressions(minLambdas);
+        src{end+1}=minLambda0;
+        dst{end+1}=minLambda;
+    else
+        minLambda=Tzeros(0);
+    end
+    % Maximizer
+    if size(Gd,1)>0
+        maxNu=packVariables(maxNus,'maxNu_');
+        maxNu0=packExpressions(maxNus);
+        src{end+1}=maxNu0;
+        dst{end+1}=maxNu;
+    else
+        maxNu=Tzeros(0);
+    end
+    if size(Fd,1)>0
+        maxLambda=packVariables(maxLambdas,'maxLambda_');
+        maxLambda0=packExpressions(maxLambdas);
+        src{end+1}=maxLambda0;
+        dst{end+1}=maxLambda;
+    else
+        maxLambda=Tzeros(0);
+    end
+    declareCopy(code,dst,src,'initPrimalDual__');
+
+    %% Generate the code for the functions that do the raw computation
+    t_ipmPD=clock();
+    Tout=ipmPDminmax_CS(struct(...
+        'code',code,...
+        'objective',objective,...
+        'u',u,...
+        'd',d,...
+        'x',x,...
+        'minLambda',minLambda,...
+        'minNu',minNu,...
+        'maxLambda',maxLambda,...
+        'maxNu',maxNu,...
+        'Fu',Fu,...
+        'Gu',Gu,...
+        'Fd',Fd,...
+        'Gd',Gd,...
+        'H',H,...
+        'smallerNewtonMatrix',smallerNewtonMatrix,...
+        'addEye2Hessian',addEye2Hessian,...
+        'skipAffine',skipAffine,...
+        'scaleInequalities',scaleInequalities,...
+        'scaleCost',scaleCost,...
+        'scaleEqualities',scaleEqualities,...
+        'useLDL',useLDL,...
+        'atomicFactorization',useUmfpack,...
+        'cmexfunction',classname,...
+        'allowSave',allowSave,...
+        'debugConvergence',debugConvergence,...
+        'profiling',profiling));
+    code.statistics.time.ipmPD=etime(clock,t_ipmPD);
+
+    % Replace solver variables into output expression
+    fn=fields(Tout);
+    for i=1:length(fn)
+        varname=sprintf('%s_',fn{i});
+        outputExpressions=substitute(outputExpressions,...
+                                     Tvariable(varname,size(Tout.(fn{i})),true),Tout.(fn{i}));
+    end
+
+    %% Declare ipm solver
+    classhelp{end+1}='Solve optimization';
+    classhelp{end+1}='[status,iter,time]=solve(obj,mu0,int32(maxIter),int32(saveIter),addEye2Hessian);';
+    defines.nZ=size(u,1)+size(d,1)+size(x,1);
+    defines.nU=size(u,1);
+    defines.nD=size(d,1);
+    defines.nX=size(x,1);
+    defines.nG=size(Gu,1)+size(Gd,1)+size(H,1);
+    defines.nF=size(Fu,1)+size(Fd,1);
+    defines.nNu=size(Gu,1)+size(Gd,1)+2*size(H,1);
+    defines.gradTolerance=gradTolerance;
+    defines.addEye2Hessian1tolerance=addEye2Hessian1tolerance;
+    defines.equalTolerance=equalTolerance;
+    defines.desiredDualityGap=desiredDualityGap;
+    defines.scaleCost=double(scaleCost);
+    defines.scaleInequalities=double(scaleInequalities);
+    defines.scaleEqualities=double(scaleEqualities);
+    defines.alphaMin=alphaMin;
+    defines.alphaMax=alphaMax;
+    defines.coupledAlphas=double(coupledAlphas);
+    defines.setAddEye2Hessian=double(addEye2Hessian~=0);
+    defines.adjustAddEye2Hessian=double(adjustAddEye2Hessian~=0);
+    defines.muFactorAggressive=muFactorAggressive;
+    defines.muFactorConservative=muFactorConservative;
+    defines.delta=delta;
+    defines.skipAffine=double(skipAffine);
+    defines.smallerNewtonMatrix=double(smallerNewtonMatrix);
+    defines.useLDL=double(useLDL);
+    defines.useUmfpack=double(useUmfpack);
+    defines.allowSave=double(allowSave);
+    defines.debugConvergence=double(debugConvergence);
+    defines.debugConvergenceThreshold=debugConvergenceThreshold;
+    defines.profiling=double(profiling);
+    defines.verboseLevel=solverVerboseLevel;
+
+    pth=fileparts(which('class2minmaxCS.m'));
+    declareFunction(code,fsfullfile(pth,'ipmPDminmax_CSsolver.m'),'solve',defines,[],[],'solve');
+
+    %% Declare 'gets' for output expressions
+    classhelp{end+1}='Get outputs';
+    classhelp{end+1}='';
+    for i=1:length(outputExpressions)
+        classhelp{end}=[classhelp{end},outputNames{i},','];
+    end
+    classhelp{end}=sprintf('[%s]=getOutputs(obj);',classhelp{end}(1:end-1));
+    classhelp{end+1}=sprintf('[y (struct)]=getOutputs(obj);',classhelp{end}(1:end-1));
+
+    declareGet(code,cell2struct(outputExpressions,outputNames),'getOutputs');
+
+    code.statistics.time.csparse=etime(clock,t_csparse);
+    code.statistics.defines=defines;
+
+    fprintf('  done creating csparse object (%.3f sec)\n',etime(clock,t_csparse));
+
+    %% Compile code
+    fprintf(' creating matlab code... ');
+    t_compile2matlab=clock();
+    compile2matlab(code,...
+                   fsfullfile(folder,sprintf('%s.m',classname)),...
+                   fsfullfile(folder,sprintf('%s.log',classname)),...
+                   classhelp,...
+                   profiling);
+    code.statistics.time.compile2matlab=etime(clock,t_compile2matlab);
+
+    fprintf(' done creating matlab code (%.3f sec)\n',etime(clock,t_csparse));
+
+    %% debug info to be passed to debugConvergenceAnalysis
+
+    debugInfo.minOptimizationVariables=minOptimizationVariables;
+    debugInfo.maxOptimizationVariables=maxOptimizationVariables;
+    debugInfo.minConstraints=minConstraints;
+    debugInfo.maxConstraints=maxConstraints;
+
+    code.statistics.time.cmexCS=etime(clock,t_classCS);
+    fprintf('done class2minmaxCS (%.3f sec)\n',etime(clock,t_classCS));
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Set outputs
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    varargout=setOutputs(nargout,params);
+
+end
---
+++ lib/private/parameters4minmax.m
@@ -0,0 +1,164 @@
+function localVariables_=parameters4minmax(localVariables_)
+% Declare input parameters common to the 2 Tenscalc functions:
+%   cmex2minmaxCS.m
+%   class2minmaxCS.m
+%
+% This file is part of Tenscalc.
+%
+% Copyright (C) 2012-22 The Regents of the University of California
+% (author: Dr. Joao Hespanha, Raphael Chinchilla).  All rights reserved.
+
+    %% Declare input parameters
+
+    declareParameter(...
+        'VariableName','objective',...
+        'Description',{
+            'Scalar Tcalculus symbolic object to be optimized.'
+                      });
+    declareParameter(...
+        'VariableName','minOptimizationVariables',...
+        'Description',{
+            'Cell-array of Tcalculus symbolic objects representing the'
+            'variables to be minimized.'
+                      });
+    declareParameter(...
+        'VariableName','minConstraints',...
+        'DefaultValue',{},...
+        'Description',{
+            'Cell-array of Tcalculus symbolic objects representing the'
+            'constraints for the minimizer. Both equality and inequality'
+            'constraints are allowed.'
+            ' '
+            'ATTENTION: These constrainsts should NOT depend on'
+            '           |maxOptimizationVariables|'
+            'TO_BE_FIXED: CODE SHOULD CHECK FOR THIS BY COMPUTING DERIVATIVES'
+                      });
+
+    declareParameter(...
+        'VariableName','maxOptimizationVariables',...
+        'Description',{
+            'Cell-array of Tcalculus symbolic objects representing the'
+            'variables to be maximized.'
+                      });
+    declareParameter(...
+        'VariableName','maxConstraints',...
+        'DefaultValue',{},...
+        'Description',{
+            'Cell-array of Tcalculus symbolic objects representing the'
+            'constraints for the maximizer. Both equality and inequality'
+            'constraints are acceptable.'
+            ' '
+            'These constraints may depend on both |minOptimizationVariables|'
+            'and |maxOptimizationVariables|'.
+                      });
+
+    declareParameter(...
+        'VariableName','outputExpressions',...
+        'Description',{
+            'Cell-array of Tcalculus symbolic objects representing the '
+            'variables to be returned.'
+            ' ';
+            'TO_BE_FIXED'
+            ' ';
+            'The following Tcalculus symbolic variables are assigned special values';
+            'and can be using in outputExpressions';
+            '* |P1lambda1_|,|P1lambda2_|,... |P2lambda1_|,|P2lambda2_|,...'
+            '      - Lagrangian multipliers associated with the inequalities constraints';
+            '        for player 1 and 2 (in the order that they appear and with the same size';
+            '        as the corresponding constraints).';
+            '* |P1nu1_|,|P1nu2_|,... ,|P2nu1_|,|P2nu2_|,...'
+            '* |P1xnu1_|,|P1xnu2_|,... ,|P2xnu1_|,|P2xnu2_|,...'
+            '      - Lagrangian multipliers associated with the equality constraints';
+            '        for player 1 and 2 (in the order that they appear and with the same size'
+            '        as the corresponding constraints). The P1x and P2x variables correspond';
+            '        to the latentConstraints.'
+            '* |Hess_| - Hessian matrix used by the (last) Newton step to update';
+            '            the primal variables (not including |addEye2Hessian|).'
+            ' ';
+            'ATTENTION: To be able to include these variables as input parameters,';
+            '           they have to be previously created outside *with the appropriate sizes*.'
+            '           Eventually, their values will be overridden by the solver'
+            '           to reflect the values listed above.'
+                      });
+
+    declareParameter(...
+        'VariableName','addEye2Hessian',...
+        'DefaultValue',false,...
+        'AdmissibleValues',{false,true},...
+        'Description',{
+            'When |true|, adds to the Newton matrix identity matrices scaled by small constants.';
+            ' ';
+            'One scaled identity matrix equal to';
+            '           addEye2Hessian1 * eye(# primal variables)'
+            'is added to the matrix of 2nd derivatives of the Lagrangian (Hessian).'
+            ' '
+            'A seconds scaled identity matrix equal to';
+            '           addEye2Hessian2 * eye(# equality constraints)'
+            'is added to the diagonal block of the Newton matrix that corresponds to the equality'
+            'constraints, which makes factorization of the Newton matrix numerically more stable.'
+            ' '
+            'Both effects improve the robustness of the solver, but may lead to slower convergence.';
+            ' ';
+            'The constants |addEye2Hessian1| and |addEye2Hessian2| can be set at solve time'
+            'through input parameters to the solve function.'
+            ' '
+            'A typical choices for |addEye2Hessian1| and |addEye2Hessian2| is the square root'
+            'of the machine precision.'
+            ' '
+            'The values of |addEye2Hessian1| and |addEye2Hessian2| can be viewed by setting'
+            '|solverVerboseLevel| to 3.'
+                      });
+
+    declareParameter(...
+        'VariableName','adjustAddEye2Hessian',...
+        'DefaultValue',false,...
+        'AdmissibleValues',{false,true},...
+        'Description',{
+            'When |true|, the values of the parameters |addEye2Hessian1| and |addEye2Hessian2|';
+            'are adjusted in real-time by the solver.';
+            ' ';
+            'ATTENTION: currently addEye2Hessian1 is not adjusted';
+                      });
+
+    declareParameter(...
+        'VariableName','addEye2Hessian1tolerance',...
+        'DefaultValue',1e-6,...
+        'Description',{
+            'When |adjustAddEye2Hessian|=|true|, waits until |addEye2Hessian1| becomes smaller than this value.';
+            ' ';
+            'This parameter is ignored when |adjustAddEye2Hessian|=|false|.'
+            ' ';
+            'ATTENTION: currently addEye2Hessian1 is not adjusted,'
+            '           so addEye2Hessian1 must be initialized below this value.';
+                      });
+
+    declareParameter(...
+        'VariableName','useLDL',...
+        'DefaultValue',false,...
+        'AdmissibleValues',{false,true},...
+        'Description',{
+            'TO_BE_FIXED, PROBABLY REMOVED'
+            ' '
+            'When |true| the search directions are computed using an'
+            'LDL instead of an LU factorization.';
+            ' '
+            'To achieve this the system of equations for the Newton step is'
+            'symmetrized, which typically makes it less sparse.'
+            ' '
+            'In general, the LDL factorization leads to faster code.';
+            'However, the current implementation is restricted to a pure';
+            'diagonal matrix (no 2x2 blocks in the D factor) so it may';
+            'fail with the message ''ldl needs pivoting''. If this happens';
+            'either set |useLDL=false| or use a nonzero value for |addEye2Hessian|.';
+            ' ';
+            'ATTENTION: This is an experimental parameter.'
+                      });
+
+end
+
+
+% LocalWords:  localVariables tenscalc cmex equilibriumLatentCS Joao
+% LocalWords:  declareParameter VariableName Tcalculus DefaultValue
+% LocalWords:  optimizationVariables latentVariables xnu addEye nd
+% LocalWords:  latentConstraints outputExpressions AdmissibleValues
+% LocalWords:  adjustAddEye LDL useLDL solverVerboseLevel
