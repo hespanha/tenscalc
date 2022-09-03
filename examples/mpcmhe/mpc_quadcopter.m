0556de714bb01b7c559923d0d56d89cd67b336e3
Joao Hespanha <hespanha@ece.ucsb.edu>
2022-09-03 12:41:13 -0700

    Added quadcopter example

---
+++ examples/mpcmhe/mpc_quadcopter.m
@@ -0,0 +1,336 @@
+% Path planning for a quadcopter with model
+%
+%    ddot p = -b_{drag} dot p + g + R_{ib} u_thrust m_thrust
+%
+% where
+%    p        = center of mass position
+%    b_{drag} = drag coefficient
+%               (assumed a scalar constant)
+%    u_thrust = unit-vector along the thrust direction in body frame
+%               (assumed constant and equal to [0;0;1] - all propellers pointing down)
+%    m_thrust = magnitude
+%    g        = gravity vector (in inertial frame)
+%               (assumed constant)
+%    R_{ib}   = rotation matrix from body to intertial frame
+%
+% All vectors assume a North-East-Down coordinate system (so z<0 means xabove ground!)
+%
+% We assume that a low level controller regulates 4DOFs:
+%
+%    1) the total thrust magnitude m_thrust (scalar),
+%
+%       subject to the following constraints
+%           m_thrust >= min_thrust   [> 0, propellers only rotate in one direction]
+%           m_thrust <= max_thrust
+%
+%    2) the orientation R_{ib} of the quadcopter (3-vector)
+%
+% To faciliate the optimization, we optimize with respect to the variable
+%
+%      u = R_{ib} u_thrust m_thrust
+%
+% which leads to the linear dynamics
+%
+%    ddot p = -b_{drag} dot p + g + u
+%
+% from which we recover the desired R_{ib} and m_thrust as follows:
+%
+%   a) u = R_{ib} [0;0;1] m_thrust
+%           => m_thrust =  \|u\|       -- should not become zero to avoid singularity
+%           => R_ib(:,3) = u/\|u\|
+%
+%   b)  y-axis of body selected to have no vertical component
+%       (i.e., pointing sideway horizontally)
+%           => R_ib(:,2) = R_ib(:,3) x [0,0,1]
+%
+%   c) R is a rotation matrix
+%           => R_ib(:,1) = R_ib(:,2) x R_ib(:,3)
+%
+% Note that the constraints will have to be imposed on these variables
+% (and their derivatives) rather than on u.
+%
+% The goal of MPC is to minimize the distance to a desired final
+% position/velocity, penalizing total thrust
+%
+%         J = \|p(T)-pfinal\|^2 + lambda1 \|dot p(T)-vfinal\|^2 + lambda2 \int_0^T m_thrust(t) dt
+%
+% subject to the constraintrs mentioned on 1) and 2) above as well as
+% a minimum altitude consraint:
+%
+%        p(3,:) <= -min_altitude    (recall that z-xis points down)
+%
+% The solver can be generated once and then called multiple
+% times. Creating a solver typically takes much longer than solving
+% the optimization by creating the solver.
+%
+% This script gives you the option of skipping creating the solver,
+% but you can only skip generating a solver if you have done it once.
+
+% This file is part of Tencalc.
+%
+% Copyright (C) 2012-21 The Regents of the University of California
+% (author: Dr. Joao Hespanha).  All rights reserved.
+
+clear all;
+% remove previous solvers
+%delete('toremove.m','tmp*');rc=rmdir('@tmp*','s');
+
+help mpc_quadcopter
+
+createSolvers=input('Do you want to create the solvers (Y/n)? ','s');
+createSolvers=~isequal(createSolvers,'n');
+
+%%%%%%%%%%%%%%%%%%%
+%% Load test data
+%%%%%%%%%%%%%%%%%%%
+
+T=100;                        % horizon length
+
+if createSolvers
+
+    %%%%%%%%%%%%%%%%%%%
+    %% Generate solver
+    %%%%%%%%%%%%%%%%%%%
+
+    Tcalculus.clear();
+
+    Tvariable Ts [];              % sampling time
+    Tvariable p [3,T];            % position:              [p(1) p(2) ... p(T)]
+    Tvariable u [3,T];            % optimization variable: [u(1) u(2) ... u(T)]
+
+    Tvariable pinit [3,1];        % initial position:       p(1)
+    Tvariable vinit [3,1];        % initial velocity:       v(1)
+    Tvariable pdesired [3,1];     % desired final position: p_{desired}(T)
+    Tvariable vdesired [3,1];     % desired final velocity: v_{desired}(T)
+
+    Tvariable b_drag [];          % drag coefficient
+    g=Tconstant([0;0;9.8],[3,1]); % gravity vector (in inertial frame)
+
+    Tvariable min_thrust [];      % minimum thrust
+    Tvariable max_thrust [];      % maximum thrust
+    Tvariable max_omega  [];      % maximum angular velocity
+    Tvariable min_altitude [];    % minimum altitude
+
+    v=tsDerivative(p,Ts);         % velocity
+    a=tsDerivative2(p,Ts);        % acceleration
+
+    % System dynamics
+    dynamics={
+        a==-b_drag*v+repmat(g,[1,T])+u;
+        p(:,1)==pinit;            % initial position
+        v(:,1)==vinit;            % initial velocity
+             };
+
+    m_thrust = sqrt(sum(u.^2,1));      % m_thurst = \|u\| from a) above -- sqrt is dangerous because not diff
+    R_ib3    = u./repmat(reshape(m_thrust,[1,T]),[3,1]); % reshape needed by repmat
+    R_ib2    = tsCross(R_ib3,[Tzeros(2,T);Tones(1,T)]);
+    R_ib1    = tsCross(R_ib2,R_ib3);
+
+    % Constraints
+    Tvariable positive2 [T];
+    constraints={
+        m_thrust >= min_thrust;
+        ...%m_thrust <= max_thrust;
+        max_thrust-m_thrust==positive2;positive2>0;  % this will work even if initial condition violates
+        p(3,:) < -min_altitude;
+                };
+
+    % Criterion
+    Tvariable lambda_v [];            % weigth for velocity error
+    Tvariable lambda_thrust [];       % weigth for thrust
+
+    Jp2=norm2(p(:,T)-pdesired);       % position error^2
+    Jv2=norm2(v(:,T)-vdesired);       % velocity error^2
+    Jp2=tsIntegral(sum((p-repmat(pdesired,[1,T])).^2,1),Ts);    % integral of position error^2
+    Jv2=tsIntegral(sum((v-repmat(vdesired,[1,T])).^2,1),Ts);    % integral of velocity error^2
+    Jthrust=tsIntegral(m_thrust,Ts);  % integral of m_thrust
+
+    J = Jp2+lambda_v*Jv2+lambda_thrust*Jthrust;
+
+    % Solver output
+    outputExpressions=struct('J',J,...
+                             'Jp2',Jp2,...
+                             'Jv2',Jv2,...
+                             'Jthrust',Jthrust,...
+                             't',(0:T-1)*Ts,...
+                             'u',u,...
+                             'm_thrust',m_thrust,...
+                             'p',p,...
+                             'v',v,...
+                             'a',a,...
+                             'R_ib1',R_ib1,...
+                             'R_ib2',R_ib2,...
+                             'R_ib3',R_ib3,...
+                             'positive2',positive2);
+
+    classname=cmex2optimizeCS('classname','tmp_quadcopter',...
+                              'objective',J,...
+                              'optimizationVariables',{p,u,positive2},...
+                              'constraints',[dynamics;constraints],...
+                              'outputExpressions',outputExpressions,...
+                              'parameters',{...
+                                  Ts;...
+                                  pinit;vinit;...
+                                  pdesired;vdesired;...
+                                  b_drag;...
+                                  min_thrust;max_thrust;min_altitude;...
+                                  lambda_v;lambda_thrust},...
+                              'addEye2Hessian',true,...
+                              'scaleInequalities',true,...
+                              'solverVerboseLevel',4);
+
+end
+
+%%%%%%%%%%%%%%%
+%% Use solver
+%%%%%%%%%%%%%%%
+
+% Create object
+obj=tmp_quadcopter();
+mu0=.001;       % low value will speed-up convergence (up to a point where it causes numerical issues)
+maxIter=1000;
+saveIter=-1;
+
+s = RandStream('mt19937ar','Seed',0); % fix seed for reproducibility
+RandStream.setGlobalStream(s);
+
+%% Set parameters
+Ts=.01;
+b_drag=.1;
+min_altitude=-.1;     % must be a little lower than initial altitude
+min_thrust=5;         % must be positive to avoid singularities in computing R_{ib}
+max_thrust=20;
+lambda_v=0.05;        % low weight just to regularize solution
+lambda_thrust=.05;    % low weight just to regularize solution
+
+setP_Ts(obj,Ts);
+setP_b_drag(obj,b_drag);
+setP_min_altitude(obj,min_altitude);
+setP_min_thrust(obj,min_thrust);
+setP_max_thrust(obj,max_thrust);
+setP_lambda_v(obj,lambda_v);
+setP_lambda_thrust(obj,lambda_thrust);
+
+
+pinit=[0;0;0];
+vinit=[0;0;0];
+pdesired=[0;10;-5]/2;
+vdesired=[0;0;0];
+setP_pdesired(obj,pdesired);
+setP_vdesired(obj,vdesired);
+
+clear history;
+for k=1:1000
+
+    setP_pinit(obj,pinit);
+    setP_vinit(obj,vinit);
+
+    %% Initialize primal variables using a spline that respects initial position and velocity
+    pfinal=pdesired;                  % try to go all the way (may not respect constraints)
+    pfinal=pinit+(pdesired-pinit)/10; % try to go only part of way (to respect constraints)
+    t=(1:T)*Ts;
+    cs=spline([t(1),t(2),t(3),t(end)],...
+              [pinit,pinit+Ts*vinit,pinit+2*Ts*vinit,pfinal]);
+    p=ppval(cs,t);
+    setV_p(obj,p);
+
+    v=tsDerivative(p,Ts);         % velocity
+    a=tsDerivative2(p,Ts);        % acceleration
+    g=[0;0;9.8];
+    u=a+b_drag.*v-g; % ddot p = -b_{drag} dot p + g + u
+    setV_u(obj,u);
+    setV_positive2(obj,ones(T,1));
+
+    % Solve optimization
+    [status,iter,time]=solve(obj,mu0,int32(maxIter),int32(saveIter));
+    % Get outputs
+    out=getOutputs(obj);
+    k
+    disp(out)
+
+    %% Save applied control
+    if k==1
+        history.t(k)=0;
+    else
+        history.t(k)=history.t(k-1)+Ts;
+    end
+    history.p(1:3,k)=pinit;
+    history.v(1:3,k)=vinit;
+    history.u(1:3,k)=u(:,1);
+    history.R_ib1(1:3,k)=out.R_ib1(:,1);
+    history.R_ib2(1:3,k)=out.R_ib2(:,1);
+    history.R_ib3(1:3,k)=out.R_ib3(:,1);
+    history.m_thrust(k)=out.m_thrust(1);
+    history.J(k)=out.J;
+    history.Jp2(k)=out.Jp2;
+    history.Jv2(k)=out.Jv2;
+    history.iter(k)=iter;
+    history.time(k)=time;
+
+    %% Apply (constant) control to update initial position and velocity
+    % ddot p = -b_{drag} dot p + g + R_{ib} u_thrust m_thrust
+    gRu=g+out.R_ib3(:,1)*out.m_thrust(1);  % 1st time
+    [tout,yout]=ode23(@(t,pv)[pv(4:6,:);-b_drag*pv(4:6,:)+gRu],[0,Ts],[pinit;vinit]);
+    pinit=yout(end,1:3)';
+    vinit=yout(end,4:6)';
+
+    if mod(k,10)==1 || status
+        %% Plot MPC solution
+        clearFigure('figureNumber',1,'figureName','latest MPC solution');
+        plotData(out,Ts)
+        %% Plot actual control
+        clearFigure('figureNumber',2,'figureName','Actual control');
+        plotData(history,Ts)
+        clearFigure('figureNumber',3,'figureName','solver');
+        subplot(2,2,1);plot(history.t,history.iter,'.-');ylabel('# solver iter');grid on;
+        subplot(2,2,2);plot(history.t,1000*history.time,'.-');ylabel('solver time [ms]');grid on;
+        subplot(2,2,3);plot(history.t,history.Jp2,'.-');ylabel('Jp2');grid on;
+        subplot(2,2,4);plot(history.t,history.Jv2,'.-');ylabel('Jv2');grid on;
+        drawnow;
+    end
+
+    if status
+        error('optimization failed\n');
+    end
+
+    %fprintf('paused\n');pause
+end
+
+
+% Delete object
+clear obj
+
+function plotData(data,Ts)
+    % 1: x-y    2: px-t  3: vx-t 4: wx-t
+    % 5: u-t    6: py-t  7: vx-t 8: wx-t
+    % 9: th-t  10: py-t 11: vx-t 12: wx-t
+    subplot(3,4,1);
+    plot(data.p(1,:),data.p(2,:),'.-');
+    xlabel('x'),ylabel('y');
+    axis square;grid on;
+    subplot(3,4,5);
+    plot(data.t,data.u,'.-');grid on
+    legend('u_x','u_y','u_z','location','best');
+    subplot(3,4,9);
+    plot(data.t,data.m_thrust,'.-');ylabel('m_{thrust}');grid on
+    subplot(3,4,2);
+    plot(data.t,data.p(1,:),'.-');ylabel('p_x');grid on
+    subplot(3,4,6);
+    plot(data.t,data.p(2,:),'.-');ylabel('p_y');grid on
+    subplot(3,4,10);
+    plot(data.t,data.p(3,:),'.-');ylabel('p_z');grid on
+    subplot(3,4,3);
+    plot(data.t,data.v(1,:),'.-');ylabel('v_x');grid on
+    subplot(3,4,7);
+    plot(data.t,data.v(2,:),'.-');ylabel('v_y');grid on
+    subplot(3,4,11);
+    plot(data.t,data.v(3,:),'.-');ylabel('v_z');grid on
+    subplot(3,4,4);
+    plot(data.t,data.R_ib3(1,:),'.-');ylabel('R_{ib}(x,down)');grid on
+    subplot(3,4,8);
+    plot(data.t,data.R_ib3(2,:),'.-');ylabel('R_{ib}(y,down)');grid on
+    subplot(3,4,12);
+    plot(data.t,data.R_ib3(3,:),'.-');ylabel('R_{ib}(z,down)');grid on
+
+
+end
